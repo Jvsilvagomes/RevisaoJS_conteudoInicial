a657dd74ea8639f51e547f0e6cf744143ead9202
joao <joao.silvassenai@gmail.com>
2025-05-07 19:51:02 -0300

    Exercicio de Revisão

---
+++ VariaveisJS/V_exercicio01.js
@@ -0,0 +1,25 @@
+// Criando variaveis
+
+let nome = "joao";
+let idade = 16;
+let anoDeNascimento = 2008;
+
+console.log(`Olá meu nome é ${nome} nasci no ano de ${anoDeNascimento} então tenho ${idade} anos.`);
+
+console.log("----------------------------------------------------------------------------");
+
+// variável booleana para pizza
+let GostaDePizza = true
+
+console.log("Gosta de Pizza? " + GostaDePizza)
+
+console.log("----------------------------------------------------------------------------");
+
+// Usando em uma condicional
+
+if(`Gosta de pizza?`){
+    console.log(`Você é team pizza 🤪`)
+}else{
+    console.log(`Você NÃO GOSTA DE PIZZA?????? 😱`)
+}
+
