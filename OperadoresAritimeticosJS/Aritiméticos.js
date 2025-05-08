635d98f6094b6cf5652421fd6823c146e2195a9a
joao <joao.silvassenai@gmail.com>
2025-05-08 18:41:00 -0300

    Revisão Aritiméticos

---
+++ OperadoresAritimeticosJS/Aritiméticos.js
@@ -0,0 +1,8 @@
+let soma = 5 + 3;        // 8
+let subtracao = 5 - 3;   // 2
+let multiplicacao = 5 * 3;  // 15
+let divisao = 6 / 2;     // 3
+let resto = 5 % 2;       // 1 (resto da divisão)
+let potencia = 2 ** 3;   // 8 (2 elevado a 3)
+
+console.log(divisao, subtracao, multiplicacao, divisao, resto, potencia )
