27aa8fd6d1cbc6fdadac90ce202dad1de80146ae
joao <joao.silvassenai@gmail.com>
2025-05-08 19:52:18 -0300

    Revisão Loops

---
+++ Loops/ForWhile.js
@@ -0,0 +1,17 @@
+// while - repete enquanto a condição for verdadeira
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+console.log(`---------------------`)
+// for - versão mais compacta
+for (let jao = 0; jao < 5; jao++) {
+  console.log(jao);
+}
+console.log(`---------------------`)
+// Percorrendo um array
+let cores = ["azul", "verde", "vermelho"];
+for (let cor = 0; cor < cores.length; cor++) {
+  console.log(cor[cor]);
+}
