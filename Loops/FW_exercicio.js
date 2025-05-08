3f28fb278e183f000a7d37f9eebb42e99ab635e3
joao <joao.silvassenai@gmail.com>
2025-05-08 20:08:28 -0300

    Exercicio de revisão

--- Loops/FW_exercicio.js
+++ Loops/FW_exercicio.js
@@ -1,6 +1,6 @@
-// Exercício 1: Use while para mostrar números de 1 a 5 no console.
-// Exercício 2: Use for para mostrar os números pares de 0 a 10.
-// Exercício 3: Percorra o array de frutas do exercício anterior usando for.
+// Exercício 1: while para mostrar números de 1 a 5 no console.
+// Exercício 2: for para mostrar os números pares de 0 a 10.
+// Exercício 3: array de frutas do exercício anterior usando for.
 let i = 1;
 
 while(i < 6){
