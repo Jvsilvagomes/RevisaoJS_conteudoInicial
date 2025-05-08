1a6d4724be5436e9417444c673e50911608a624e
joao <joao.silvassenai@gmail.com>
2025-05-08 18:45:18 -0300

    Revisão lógicos

---
+++ OperadoresLógicos/Lógicos.js
@@ -0,0 +1,11 @@
+//(&&) - todas as condições devem ser verdadeiras
+true && true;   // true
+true && false;  // false
+
+// OR (||) - pelo menos uma condição deve ser verdadeira
+true || false;  // true
+false || false; // false
+
+// NOT (!) - inverte o valor
+!true;  // false
+!false; // true
