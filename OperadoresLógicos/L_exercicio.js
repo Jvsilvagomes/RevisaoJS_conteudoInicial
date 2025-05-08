c37ef426cae85ae0f2e84eee76f0036915d4ba3b
joao <joao.silvassenai@gmail.com>
2025-05-08 19:10:32 -0300

    Exercicio de revisão

---
+++ OperadoresLógicos/L_exercicio.js
@@ -0,0 +1,11 @@
+// Avalie estas expressões no console:
+// true && false
+// false || true
+// !(5 > 3)
+// (10 > 5) && (3 < 1)
+
+
+console.log(`true && false = `, true && false );
+console.log(`true && false = `, true || false );
+console.log(`true && false = `, !(5 > 3));
+console.log(`true && false = `, (10 > 5) && (3 < 1) );
