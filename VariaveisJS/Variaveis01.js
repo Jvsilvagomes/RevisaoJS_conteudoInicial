d40d7554efd4fc068fd0adcb6888a046dfe28ed9
joao <joao.silvassenai@gmail.com>
2025-05-08 19:15:28 -0300

    Revisão variaveis

--- VariaveisJS/Variaveis01.js
+++ VariaveisJS/Variaveis01.js
@@ -2,4 +2,4 @@ let nome = "joao";
 let idade = 16;
 let hobbie = "Volei";
 
-console.log(`Meu nome é ${nome} e tenho ${idade} anos, meu hobbie preferido é ${hobbie}.`)
+console.log(`Meu nome é ${nome} e tenho ${idade} anos, meu hobbie preferido é ${hobbie}.`);
