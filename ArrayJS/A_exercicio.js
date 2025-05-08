a988de19eeae160f7d7e7e3c7f5aacaa6f746b65
joao <joao.silvassenai@gmail.com>
2025-05-08 19:47:27 -0300

    Exercicio de revisão

---
+++ ArrayJS/A_exercicio.js
@@ -0,0 +1,13 @@
+// Exercício 1: Crie um array com 5 frutas e:
+// Adicione mais 2 frutas
+// Remova a última fruta
+// Mostre o tamanho do array
+
+let frutas = ["Banana", "Manga","Melancia", "Melão", "uva"];
+
+frutas.push("amora");
+frutas.push("ameixa");
+frutas.pop();
+frutas.length;
+
+console.log(frutas.length)
