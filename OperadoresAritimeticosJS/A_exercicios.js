861bf4bcc9be5c2b0dbe708c382607bc8737c8d9
joao <joao.silvassenai@gmail.com>
2025-05-08 19:16:16 -0300

    Exercicion de revisão

--- OperadoresAritimeticosJS/A_exercicios.js
+++ OperadoresAritimeticosJS/A_exercicios.js
@@ -1,7 +1,7 @@
 // Calculando e mostrando no console:
 
 //  SOMA
-console.log(`SOMANDO`)
+console.log(`SOMANDO`);
 console.log(`Quanto é 15 + 8?`);
 
 let numb1 = 15;
@@ -13,7 +13,7 @@ console.log(`O resultado da soma de 15 e 8 é igual a ${numb1 + numb2}!`);
 console.log(`---------------------------------------------`);
 
 // SUBTRAÇÃO
-console.log(`SUBTRAINDO`)
+console.log(`SUBTRAINDO`);
 console.log(`Quanto é 20 - 5?`);
 
 let numb3 = 20;
@@ -25,7 +25,7 @@ console.log(`O resultado da subtração de 20 - 5 é de ${numb3 - numb4}!`);
 console.log(`---------------------------------------------`);
 
 // MULTIPLICAÇÃO
-console.log(`MULTIPLICANDO`)
+console.log(`MULTIPLICANDO`);
 console.log(`Quanto é 7 x 3?`);
 
 let numb5 = 30;
@@ -37,13 +37,13 @@ console.log(`O resultado da multiplicação é de ${numb5 * numb6}`);
 console.log(`---------------------------------------------`);
 
 // DIVISÃO
-console.log(`DIVIDINDO`)
+console.log(`DIVIDINDO`);
 console.log(`Quanto é 24 / 2`);
 
-let numb7 = 24
-let numb8 = 2
+let numb7 = 24;
+let numb8 = 2;
 
-console.log(`RESPOSTA:`)
-console.log(`O resultado da divisão é de ${numb7 / numb8}`)
+console.log(`RESPOSTA:`);
+console.log(`O resultado da divisão é de ${numb7 / numb8}`);
 
 
